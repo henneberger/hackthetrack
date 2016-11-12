af5e3043953637f9d0ea0f640a7653ce657a58f4
Daniel Henneberger <henneberger@addthis.com>
2016-11-12 10:43:54 -0500

    moar

---
+++ src/main/java/model/Positions.java
@@ -0,0 +1,7 @@
+package model;
+
+import java.util.List;
+
+public class Positions {
+    public List<TrainPosition> trainPositions;
+}
---
+++ src/main/java/model/TrainPosition.java
@@ -0,0 +1,11 @@
+package model;
+
+public class TrainPosition {
+    public String TrainId;
+    public int CarCount;
+    public int DirectionNum;
+    public int CircuitId;
+    public String DestinationStationCode;
+    public int SecondsAtLocation;
+    public String ServiceType;
+}
